eb6a56ce99d044429c51505d07021dd06b0a764c
Osval Montesinos <omontesinos@ucol.mx>
2025-01-16 15:36:34 -0600

    MTT V.91

--- MTT/mtt.js
+++ MTT/mtt.js
@@ -1,122 +1,123 @@
 var label = document.getElementById('labelForMorse');
-var text = document.getElementById('textToMorseCode');
+var textValue = document.getElementById('textToMorseCode');
 var textOption = document.getElementById('textOption');
 var morseOption = document.getElementById('morseOption');
 
 function isChecked() {
+    const text = textValue.value.trim();
     if (textOption.checked) {
-      if(text.value.charAt(0) === '-' || text.value.charAt(0) === '.') {
-      label.innerText = 'The given sentence does not contanin any text';
-      return;
-      }
-      function encryption() {
-          let letterToMorseCode = { 
-            'A': '.-',
-            'B': '-...',
-            'C': '-.-.',
-            'D': '-..',
-            'E': '.',
-            'F': '..-.',
-            'G': '--.',
-            'H': '....',
-            'I': '..',
-            'J': '.---',
-            'K': '-.-',
-            'L': '.-..',
-            'M': '--',
-            'N': '-.',
-            'O': '---',
-            'P': '.--.',
-            'Q': '--.-',
-            'R': '.-.',
-            'S': '...',
-            'T': '-',
-            'U': '..-',
-            'V': '...-',
-            'W': '.--',
-            'X': '-..-',
-            'Y': '-.--',
-            'Z': '--..',
-            '0': '-----',
-            '1': '.----',
-            '2': '..---',
-            '3': '...--',
-            '4': '....-',
-            '5': '.....',
-            '6': '-....',
-            '7': '--...',
-            '8': '---..',
-            '9': '----.',
-          };
-          let result = '';
-          for (let i = 0; text.value.length > i; i++) {
-              const currentChar = text.value.charAt(i).toUpperCase();
-            if (currentChar in letterToMorseCode) {
-              result += letterToMorseCode[currentChar]+'/';
-            } else {
-              result += '/';
+        if (text.charAt(0) === '-' || text.charAt(0) === '.') { 
+            label.innerText = 'The given sentence does not contain any text';
+            return;
+        }
+        function encryption() {
+            let letterToMorseCode = { 
+                'A': '.-',
+                'B': '-...',
+                'C': '-.-.',
+                'D': '-..',
+                'E': '.',
+                'F': '..-.',
+                'G': '--.',
+                'H': '....',
+                'I': '..',
+                'J': '.---',
+                'K': '-.-',
+                'L': '.-..',
+                'M': '--',
+                'N': '-.',
+                'O': '---',
+                'P': '.--.',
+                'Q': '--.-',
+                'R': '.-.',
+                'S': '...',
+                'T': '-',
+                'U': '..-',
+                'V': '...-',
+                'W': '.--',
+                'X': '-..-',
+                'Y': '-.--',
+                'Z': '--..',
+                '0': '-----',
+                '1': '.----',
+                '2': '..---',
+                '3': '...--',
+                '4': '....-',
+                '5': '.....',
+                '6': '-....',
+                '7': '--...',
+                '8': '---..',
+                '9': '----.',
+            };
+            let result = '';
+            for (let i = 0; text.length > i; i++) { 
+                const currentChar = text.charAt(i).toUpperCase();
+                if (currentChar in letterToMorseCode) {
+                    result += letterToMorseCode[currentChar] + '/';
+                } else {
+                    result += '/';
+                }
             }
-          }
-          label.innerText = result.trim();
+            label.innerText = result.trim();
         }
         encryption();
     } else if (morseOption.checked) {
-      if(text.value.charAt(0) !== '-' && text.value.charAt(0) !== '.') {
-        label.innerText = 'The given sentence does not contanin any morse code';  
-        return;
-      }
-      function decrypt () {
-        const morseCodeToLetter = { 
-          '.-': 'a',
-          '-...': 'b',
-          '-.-.': 'c',
-          '-..': 'd',
-          '.': 'e',
-          '..-.': 'f',
-          '--.': 'g',
-          '....': 'h',
-          '..': 'i',
-          '.---': 'j',
-          '-.-': 'k',
-          '.-..': 'l',
-          '--': 'm',
-          '-.': 'n',
-          '---': 'o',
-          '.--.': 'p',
-          '--.-': 'q',
-          '.-.': 'r',
-          '...': 's',
-          '-': 't',
-          '..-': 'u',
-          '...-': 'v',
-          '.--': 'w',
-          '-..-': 'x',
-          '-.--': 'y',
-          '--..': 'z',
-          '-----': '0',
-          '.----': '1',
-          '..---': '2',
-          '...--': '3',
-          '....-': '4',
-          '.....': '5',
-          '-....': '6',
-          '--...': '7',
-          '---..': '8',
-          '----.': '9'
-        };
-        const message = text.value.split('/'); 
-        let result = '';
-        for (let i = 0; message.length > i; i++) {
-          const currentChar = message[i];
-          if (currentChar in morseCodeToLetter) {
-            result += morseCodeToLetter[currentChar];
-          } else {
-            result += ' ';
-          }
+        if (text.charAt(0) !== '-' && text.charAt(0) !== '.') { 
+            label.innerText = 'The given sentence does not contain any morse code';  
+            return;
         }
-        label.innerText = result.charAt(0).toUpperCase() + result.slice(1);
-      }
-      decrypt();
+        function decrypt() {
+            const morseCodeToLetter = { 
+                '.-': 'a',
+                '-...': 'b',
+                '-.-.': 'c',
+                '-..': 'd',
+                '.': 'e',
+                '..-.': 'f',
+                '--.': 'g',
+                '....': 'h',
+                '..': 'i',
+                '.---': 'j',
+                '-.-': 'k',
+                '.-..': 'l',
+                '--': 'm',
+                '-.': 'n',
+                '---': 'o',
+                '.--.': 'p',
+                '--.-': 'q',
+                '.-.': 'r',
+                '...': 's',
+                '-': 't',
+                '..-': 'u',
+                '...-': 'v',
+                '.--': 'w',
+                '-..-': 'x',
+                '-.--': 'y',
+                '--..': 'z',
+                '-----': '0',
+                '.----': '1',
+                '..---': '2',
+                '...--': '3',
+                '....-': '4',
+                '.....': '5',
+                '-....': '6',
+                '--...': '7',
+                '---..': '8',
+                '----.': '9'
+            };
+            const message = text.split('/'); 
+            let result = '';
+            for (let i = 0; message.length > i; i++) {
+                const currentChar = message[i].trim();
+                if (currentChar in morseCodeToLetter) {
+                    result += morseCodeToLetter[currentChar];
+                } else {
+                    result += ' ';
+                }
+            }
+            label.innerText = result.charAt(0).toUpperCase() + result.slice(1);
+        }
+        decrypt();
     } else {
         label.innerText = 'You must select one of the options';
     }
